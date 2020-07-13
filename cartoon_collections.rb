031f8b83b414209ef96387016c5cf599451109f5
Russell Sutter <russell.sutter@gmail.com>
2020-07-13 19:35:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   
-  array.each_with_index |element|
+  array.each_with_index do |element|
    puts "#{element}"
  end
 end
